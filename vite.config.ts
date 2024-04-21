2139ad84f64148fd61baa61b0e23a9a5486a539b
hywax <me@hywax.space>
2024-04-21 20:01:02 +0500

    chore: vite config

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+/// <reference types="vitest" />
+
+import path from 'node:path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ['phaser'],
+        },
+      },
+    },
+  },
+  esbuild: {
+    legalComments: 'none',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    globals: true,
+    reporters: ['default', 'junit'],
+    outputFile: {
+      junit: 'dist/test-reports/junit.xml',
+    },
+    coverage: {
+      reporter: ['clover', 'text'],
+      reportsDirectory: 'dist/test-reports',
+      include: ['src/**.{ts}', 'src/**/**.{ts}'],
+      exclude: ['node_modules/'],
+      cleanOnRerun: true,
+      clean: true,
+      all: true,
+      provider: 'v8'
+    },
+  },
+})
