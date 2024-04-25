9dbc960638e4756a4213131c2ec4c8a17541e7cd
hywax <me@hywax.space>
2024-04-25 13:53:58 +0500

    chore: game class

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { Game } from 'phaser'
+import { FontPlugin } from 'phaser-font-plugin'
+import { Boot, Menu, Preloader } from '@/scenes'
 
 export default new Game({
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   parent: 'game',
+  backgroundColor: '#4dc0ca',
+  scene: [
+    Boot,
+    Preloader,
+    Menu,
+  ],
+  plugins: {
+    global: [
+      {
+        key: 'FontLoaderPlugin',
+        plugin: FontPlugin,
+        start: true,
+      },
+    ],
+  },
 })
